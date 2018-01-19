e6076856c0cefd6be7d66dd73b0f45b05a024b50
Bastien <bastien.durand@insa-lyon.fr>
2018-01-19 14:42:08 +0100

    main commenté

--- main.cpp
+++ main.cpp
@@ -2,12 +2,20 @@
 
 using namespace std;
 
+/*typedef Catalogue*/
+
 void printUsages();
 void genererCatalogue();
 void genererGraphe();
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	/*gérer les erreurs propres au main*/
+
+	/*générer catalogue*/
+
+	/*en fonction des arguments générerGraphe ou pas
+	puis affichage de la réponse*/
+	
 	return 0;
 }
